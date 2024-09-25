da2c3cf9e46d5b75ad72d9b46d732696540839e6
mir-asif01 <asifbsb2002@gmail.com>
2024-09-25 07:53:49 +0600

    nav-ul css changed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-const time_id = document.getElementById("time-li")
+// const time_id = document.getElementById("")
 
-const current_time = new Date().toLocaleDateString()
-time_id.textContent = current_time
+// const current_time = new Date().toLocaleDateString()
+// time_id.textContent = current_time
+
+function updateTime() {
+  const timeElement = document.getElementById("time-li")
+  const now = new Date()
+  const formattedTime = now.toLocaleTimeString() // Get time in HH:MM:SS format
+  timeElement.textContent = formattedTime
+}
+
+// Call updateTime immediately and then every second
+setInterval(updateTime, 1000)
+updateTime()
